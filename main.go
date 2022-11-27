860e623a6a7cbf54aec1518fc5c0436799c6acf0
dotNate <somersetnathaniel@gmail.com>
2022-11-27 12:17:54 +0000

    played around with gorm model

--- main.go
+++ main.go
@@ -11,5 +11,18 @@ func main() {
 	dsn := "user:mypassword@tcp(127.0.0.1:3306)/people?charset=utf8mb4&parseTime=True&loc=Local"
 	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
+	person := Person{
+		FirstName: "Nathaniel",
+		LastName:  "Payne",
+	}
+	// db.AutoMigrate(&Person{})
+	db.Create(&person)
+
 	fmt.Printf("%+v", db)
+}
+
+type Person struct {
+	gorm.Model
+	FirstName string
+	LastName  string
 }
